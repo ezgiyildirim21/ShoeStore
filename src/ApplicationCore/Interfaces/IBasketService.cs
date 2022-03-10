715cee5114dc87a38caa98b2100cbb19addf81a6
ezgiyildirim21 <ezgiyildirim38@gmail.com>
2022-03-10 12:54:25 +0300

    update, remove basket

--- src/ApplicationCore/Interfaces/IBasketService.cs
+++ src/ApplicationCore/Interfaces/IBasketService.cs
@@ -10,5 +10,11 @@ namespace ApplicationCore.Interfaces
     public interface IBasketService
     {
         Task<Basket> AddItemToBasketAsync(int basketId, int productId, int quantity);
+
+        Task<Basket> SetQuantitiesAsync(int basketId, Dictionary<int, int> quantities);
+        // * bu sepetteki   * bu id'li ürün adedini   *bu id ile güncelle
+        Task DeleteBasketAsync(int basketId);
+
+        Task DeleteBasketItemAsync(int basketId, int basketItemId);
     }
 }
